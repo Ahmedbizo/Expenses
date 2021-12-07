c1755bfa6ab57bb60181d61280300dd5a5e0d497
Devo App <57620248+Ahmedbizo@users.noreply.github.com>
2021-12-07 01:45:52 +0100

    added details to expenses component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem () {
+    const expenseDate = new Date(2021,2,28);
+    const expenseTitle = "Car Inturance ";
+    const expenseAmount = "290.34";
     return (
         <div className="expense-item">
-         <div>March 28th 2021 </div>
-          <div>
-           <h2 className="expense-item__description ">Car inturance </h2>
-           <div className="expense-item__price "> 290.34$</div>
+         <div>{expenseDate.toISOString()} </div>
+          <div className="expense-item__description ">
+           <h2 >{expenseTitle}</h2>
+           <div className="expense-item__price "> {expenseAmount}</div>
          </div>
         </div>
     )
