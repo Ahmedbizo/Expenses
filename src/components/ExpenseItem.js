78dd8d3372ba9d5067c7e29d1ff9ac258a52fae1
Devo App <57620248+Ahmedbizo@users.noreply.github.com>
2021-12-07 01:16:19 +0100

    added details to expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem () {
-    return <h2>Expenses </h2>
+    return (
+        <div>
+        <div>March 28th 2021 </div>
+        <h3>Expenses </h3>
+        <div>Car inturance </div>
+        </div>
+    )
 }
 
-export default ExpenseItem;
+export default ExpenseItem; 
